beb376e4e1ffd1fdf0b5f6b639c192f17b069598
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-09-02 14:10:48 -0500

    started js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 // jquery ready fuction
 $(document).ready(function() {
+
+    // capture the text input when search button is clicked
+    $("#search-button").on("click", function () {
+        event.preventDefault();
+        var searchInput = $("#search-input").val();
+        $("#search-input").val("");
+        $("input:text").click(function () {
+            $(this).val("");
+            $("#todays-weather").empty();
+            $("#forecast").empty();
+        });
+        // need to call weather functionName(searchInput);
+    });
+
     
 })
